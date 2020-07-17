51e82e458263dc1a7583ca5557f5aa28173a1aa6
SajalRajoria <sajalrajoria2006@gmail.com>
2020-07-17 19:46:57 +0530

    Vansh pigs

--- Slingshot.js
+++ Slingshot.js
@@ -17,7 +17,9 @@ class SlingShot{
     fly(){
         this.sling.bodyA = null;
     }
-
+    attach(body){
+        this.sling.bodyA = body
+   }
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,20);
